f4a69109f3b1cbd587b4dd8d86be12b8b1ff66eb
csenapati12 <csenapati12@gmail.com>
2023-04-25 08:45:35 +0530

    added files

--- jenkinsfile1.groovy
+++ jenkinsfile1.groovy
@@ -4,12 +4,12 @@ pipeline {
     stages {
         stage('Stage 1') {
             steps {
-                // Define steps for Stage 1
+               echo "stage1"
             }
         }
         stage('Stage 2') {
             steps {
-                // Define steps for Stage 2
+                 echo "stage2"
             }
         }
     }
--- jenkinsfile2.groovy
+++ jenkinsfile2.groovy
@@ -2,12 +2,12 @@
 stages {
     stage('Stage 3') {
         steps {
-            // Define steps for Stage 3
+             echo "stage3"
         }
     }
     stage('Stage 4') {
         steps {
-            // Define steps for Stage 4
+             echo "stage4"
         }
     }
 }
